95e849c7cef661308d1d9f721748c280f0b6d1ec
Jhon Podesta <tchikiboum@42.fr>
2026-09-22 18:23:18 +0200

    ff

--- build/vendor/yaml-cpp/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bernardo/Documents/42/M5/TheAnswerProtocol/vendor/yaml-cpp/util
-# Build directory: /home/bernardo/Documents/42/M5/TheAnswerProtocol/build/vendor/yaml-cpp/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
